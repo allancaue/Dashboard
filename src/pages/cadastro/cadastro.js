b07404bd2a2f65740e3078730094b5b2a7371eff
Allan Cauê R. Moura <allancaue@gmail.com>
2024-04-10 13:32:49 -0300

    feat: :wrench: Add links via JavaScrpt
    agora é pocivel navegar entre login e cadastro via useNavegete

--- src/pages/cadastro/cadastro.js
+++ src/pages/cadastro/cadastro.js
@@ -2,16 +2,24 @@ import Botao from "../../components/Botao/botao";
 import styles from "./cadastro.module.css"
 import Detalhe from "./img/Detalhe.svg"
 import MenuCadastro from "../../components/Cadastro/menuCadastro";
+import {useNavigate} from "react-router-dom"
 
 function Cadastro() {
+
+    const navigete = useNavigate();
+
+    const handleCadastroClick = () => {
+        navigete('/');
+    }
+
     return(
         <div className={styles.contener}>
             <div className={styles.blocoImfo}>
                 <div className={styles.imfo}>
                     <div>
-                        <h1>Titulo Teste</h1>
-                        <p>TesteTesteTesteTesteTesteTesteTesteTeste</p>
-                        <Botao nome = "Ler Mais" />
+                        <h1>Já tem uma conta</h1>
+                        <p>Entre agora pelo seu login</p>
+                        <Botao nome = "Login" onClick={handleCadastroClick}/>
                     </div>
                 </div>
                 <div className={styles.imagemDetalhe}>
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -2,16 +2,23 @@ import styles from './login.module.css';
 import Detalhe from './img/Detalhe.svg'
 import Botao from '../../components/Botao/botao';
 import Menulogin from '../../components/Login/menuLogin';
+import {useNavigate} from "react-router-dom"
 
 function Login() {
+    const navigete = useNavigate();
+
+    const handleCadastroClick = () => {
+        navigete('/cadastro');
+    }
+
     return(
         <div className={styles.contener}>
             <div className={styles.blocoImfo}>
                 <div className={styles.imfo}>
                     <div>
-                        <h1>Titulo Teste</h1>
-                        <p>TesteTesteTesteTesteTesteTesteTesteTeste</p>
-                        <Botao nome = "Ler Mais" />
+                        <h1>Cadastro aqui</h1>
+                        <p>Caso vc ainda não seja cadastrdo</p>
+                        <Botao nome = "Cadastro" onClick={handleCadastroClick} />
                     </div>
                 </div>
                 <div className={styles.imagemDetalhe}>
